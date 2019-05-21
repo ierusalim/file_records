1b1192b113ea8629cf83986eb6b46d3da6a9bf56
ierusalim <alex@ierusalim.com>
2019-05-21 16:29:53 +0300

    Init

---
+++ src/FileRecords.php
@@ -0,0 +1,217 @@
+<?php
+namespace ierusalim\FileRecords;
+
+/**
+ * This class contains FileRecords
+ *
+ * PHP Version >= 5.6
+ *
+ * @package    ierusalim\FileRecords
+ * @author     Alexander Jer <alex@ierusalim.com>
+ * @copyright  2018, Ierusalim
+ * @license    https://opensource.org/licenses/Apache-2.0 Apache-2.0
+ */
+class FileRecords
+{
+    public $file_name;
+    public $rec_size;
+    public $rec_cnt;
+    public $file_size;
+    public $f = false; // fopen-descriptor (false if not open)
+    public $f_mode;
+
+    /**
+     * Constructor, for example:
+     *
+     * $fr = new \ierusalim\FileRecords("path/file.dat", 64);
+     *
+     * @param string $file_name
+     * @param integer $rec_size
+     * @throws Exception
+     */
+    public function __construct($file_name, $rec_size)
+    {
+        if (
+            (!is_numeric($rec_size))
+            ||
+            ($rec_size != (int)$rec_size)
+            ||
+            ($rec_size < 1)
+        ) {
+            throw Exception("Record size must be integer and grater than 0");
+        }
+        $this->file_name = $file_name;
+        $this->rec_size = (int)$rec_size;
+    }
+
+    /**
+     * Return count of records in file
+     * Calculate from file-size and record-size
+     * Return 0 if no records (or no file)
+     *
+     * @return integer
+     */
+    public function recordsCount()
+    {
+        if (!$this->f) {
+            $this->file_size = @filesize($this->file_name);
+            if (!$this->file_size) {
+                $this->file_size = 0;
+            }
+            $this->rec_cnt = $this->file_size / $this->rec_size;
+        }
+        return $this->rec_cnt;
+    }
+
+    /**
+     * Read Record from file by specified number
+     *
+     * @param int $rec_num Record number
+     * @return string|false Data string (or false if error)
+     * @throws Exception
+     */
+    public function readRecord($rec_num)
+    {
+        if (!$this->f) {
+            if (!$this->fopen('r', false)) {
+                throw new Exception("Can't open file");
+            }
+        }
+        if ($rec_num >= $this->rec_cnt) {
+            return false;
+        }
+        fseek($this->f, $this->rec_size * $rec_num);
+        $data = fread($this->f, $this->rec_size);
+        return $data;
+    }
+
+    /**
+     * Close current file
+     */
+    public function fclose() {
+        if ($this->f) {
+            fclose($this->f);
+        }
+        $this->f = false;
+    }
+
+    /**
+     * Open file for Read, Append or reWrite mode.
+     *
+     * @param int $new_f_mode r = Readonly, a = Append, w = reWrite
+     * @param boolean $re_open true for close opened file and open again
+     */
+    public function fopen($new_f_mode, $re_open = false)
+    {
+        switch ($new_f_mode) {
+        case 'r':
+        case 'rb':
+            $f_mode = 'rb';
+            break;
+        case 'a':
+        case 'ab+':
+            $f_mode = 'ab+';
+            break;
+        case 'w':
+        case 'rb+':
+            $f_mode = 'rb+';
+            break;
+        default:
+            throw new Exception("Unrecognized mode: $f_mode \nSupported: r, a, w");
+        }
+
+        // if file already open
+        if ($this->f) {
+            if (
+                !$re_open
+             && (($this->f_mode === $new_f_mode) || ($new_f_mode === 'rb'))
+            ) {
+                return $this->f;
+            }
+            $this->fclose();
+        }
+
+        $this->recordsCount();
+
+        $this->f = fopen($this->file_name, $f_mode);
+        if (!$this->f) {
+            return false;
+        }
+        $this->f_mode = $f_mode;
+        return $this->f;
+    }
+
+    /**
+     * Append new record to the end of file
+     *
+     * Returns: integer record number or string error description
+     *
+     * @param string $data Record data
+     * @return integer
+     * @throws Exception
+     */
+    public function appendRecord($data)
+    {
+        $size = strlen($data);
+        if ($size != $this->rec_size) {
+            throw new Exception("Different record size: $size (need {$this->rec_size})");
+        }
+        $f = $this->fopen('ab+', false);
+        if (!$f) {
+            return "ERROR: Can't open file for append";
+        }
+
+        $rec_nmb = $this->rec_cnt;
+
+        $bcnt = fwrite($f, $data, $size);
+        if ($bcnt === false) {
+            return "ERROR: Can't write record #{$rec_nmb} to file";
+        }
+        if ($bcnt !== $size) {
+            return "ERROR: Size of writed-data #{$rec_nmb} is $bcnt different of $size";
+        }
+        $this->rec_cnt++;
+        $this->file_size += $size;
+        return $rec_nmb;
+    }
+
+    /**
+     * ReWrite existing record in file by record number
+     *
+     * Returns: integer record number or string error description
+     *
+     * @param integer $rec_num
+     * @param string $data
+     * @return integer|string
+     * @throws Exception
+     */
+    public function reWriteRecord($rec_num, $data)
+    {
+        $size = strlen($data);
+        if ($size != $this->rec_size) {
+            throw new Exception("Different record size: $size (need {$this->rec_size})");
+        }
+        $rec_max = $this->recordsCount() - 1;
+        if (($rec_num < 0) || ($rec_num > $rec_max)) {
+            throw new Exception("Record #$rec_num out of records range [0 - $rec_max)");
+        }
+
+        $f = $this->fopen('rb+', false);
+        if (!$f) {
+            return "ERROR: Can't open file for re-write";
+        }
+
+        $seek_pos = $rec_num * $this->rec_size;
+
+        fseek($f, $seek_pos);
+
+        $bcnt = fwrite($f, $data, $size);
+        if ($bcnt === false) {
+            return "ERROR: Can't write record #{$rec_nmb} to file";
+        }
+        if ($bcnt !== $size) {
+            return "ERROR: Size of writed-data #{$rec_nmb} is $bcnt different of $size";
+        }
+        return $rec_num;
+    }
+}
